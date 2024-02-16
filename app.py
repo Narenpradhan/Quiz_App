fb799af1d1ab4b34da401b7a6ef7c9fa8d794926
Naren Pradhan <narenpradhan05@gmail.com>
2024-02-17 02:42:15 +0530

    add details page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def quiz_app():
-    st.title("Quiz App")
+    st.markdown("<h1 style='text-align: center;'>IEEE Induction Quiz</h1>", unsafe_allow_html=True)
+    st.markdown("---")
 
     # Define your quiz questions and answers
     questions = [
@@ -35,5 +36,42 @@ def quiz_app():
     # Display final score
     st.write(f"Your final score is: {score}/{len(questions)}")
 
+def page_1():
+    st.markdown("<h1 style='text-align: center;'>Fill Up Your Details</h1>", unsafe_allow_html=True)
+    st.markdown("---")
+    st.write("")
+    st.write("")
+    name = st.text_input("Enter Your Full Name", placeholder="Hritesh Roshan Mahapatra")
+    st.write("")
+    roll = st.text_input("Enter Your Registration Number", placeholder="1234567890")
+    st.write("")
+    email = st.text_input("Enter Your Email", placeholder="example@email.com")
+    st.write("")
+    branch = st.radio("Select Your Branch", ["Computer Science and Engineering", "Chemical Engineering", "Civil Engineering", "Electrical Engineering", "Electrical and Electronics Engineering", "Electronics and Telecommunication Engineering", "Information Technology", "Mechanical Engineering", "Metallurgical and Materials Engineering", "Production Engineering"],index=None)
+    st.write("")
+    return name, roll, email, branch
+
 if __name__ == "__main__":
-    quiz_app()
+    if 'page' not in st.session_state:
+        st.session_state.page = 1
+
+    if st.session_state.page == 1:
+        name, roll, email, branch = page_1()
+        submitted = st.button("Next")
+        if submitted:
+            if not name:
+                st.write("Please enter your name.")
+            elif not roll:
+                st.write("Please enter your registration number.")
+            elif not email:
+                st.write("Please enter your e-mail.")
+            elif not branch:
+                st.write("Please select your branch.")
+            else:
+                st.session_state.page = 2
+
+    elif st.session_state.page == 2:
+        quiz_app()
+        st.write("")
+        if st.button("Previous"):
+            st.session_state.page = 1
